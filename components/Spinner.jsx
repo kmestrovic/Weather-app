62636f42b0011d4ea9dc9d9055ee3d2310157429
Katarina <kmarc591@gmail.com>
2024-01-07 21:47:06 +0100

    init

---
+++ components/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import spinner from '../public/spinner.gif'
+
+const Spinner = () => {
+  return (
+    <div><Image className='w-[200px] m-auto block' src={spinner}alt='loading'/></div>
+  )
+}
+
+export default Spinner
